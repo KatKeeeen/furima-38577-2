bb5ae89ae1d7dde345858cae3cc38fd1625877ba
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-01-24 21:03:23 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid.vInclude both letters and numbers"}
     validates :nickname
     validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
