9fafeadc64744491a7e6d0e2d29369899fb38856
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-01-25 21:02:08 +0900

    Faker編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,10 @@ FactoryBot.define do
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    family_name { person.last.kanji }
-    first_name { person.first.kanji }
-    family_name_kana { person.last.katakana }
-    first_name_kana { person.first.katakana }
+    family_name {Faker::Japanese::Name.last_name}
+    first_name {Faker::Japanese::Name.first_name}
+    family_name_kana {family_name.yomi}
+    first_name_kana {first_name.yomi}
+    birth_day {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
