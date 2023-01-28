f8d3acebee7ab393663e1970e5a254162c13ff89
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-01-28 22:17:27 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to  :user
   belongs_to  :category
+  belongs_to  :state
+  belongs_to  :shopping_charge
+  belongs_to  :prefecture
+  belongs_to  :delivery_time
 end
