9c73c07375ffcf8922b6e475568e8ce4b657072f
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-01-28 23:52:53 +0900

    createアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
 
   def index
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :price, :description, :category_id, :state_id, :shopping_charge_id, :prefecture_id, :delivery_time_id, :image).merge(user_id: current_user.id)
+    end
+    
+    def move_to_index
+      unless user_signed_in?
+        redirect_to action: :index
+      end
+    end
 
 end
